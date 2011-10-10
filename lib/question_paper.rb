3d9123d70f06b6da501816a957127a2ed596dacd
auoheheoo <chakwenc@cs.man.ac.uk>
2011-10-10 11:49:02 +0100

    four test cases now

--- lib/question_paper.rb
+++ lib/question_paper.rb
@@ -10,4 +10,14 @@ class QuestionPaper
   def add question
     @questions.push question
   end
+
+  def total_marks
+    total_marks = 0
+
+    @questions.each do |aQuestion|
+      total_marks += aQuestion.mark
+    end
+
+    total_marks
+  end
 end
--- spec/question_paper_spec.rb
+++ spec/question_paper_spec.rb
@@ -2,7 +2,6 @@ require "rspec"
 require "question_paper"
 
 describe QuestionPaper do
-  subject {QuestionPaper.new}
 
   it "should have an empty array of questions when QuestionPaper is created" do
     subject.questions.should be_an_instance_of Array
@@ -15,5 +14,25 @@ describe QuestionPaper do
     subject.questions.should include question
   end
 
+  it "should compute the total available marks" do
+    first_question = mock 'Question'
+    first_question.stub!(:mark).and_return(20)
+    subject.add first_question
+
+    second_question = mock 'Question'
+    second_question.stub!(:mark).and_return(10)
+    subject.add second_question
+
+    subject.total_marks.should == 30
+  end
+
+  it "should call total_marks on the question in the paper" do
+    question = mock 'Question'
+    question.stub!(:mark).and_return(10)
+    subject.add question
+
+    question.should_receive(:mark)
+    subject.total_marks
+  end
 
 end
