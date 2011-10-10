c6efbaf831cbf4e0b9fca90d87518784a5c092ac
auoheheoo <chakwenc@cs.man.ac.uk>
2011-10-10 12:00:25 +0100

    refactoring part 2

--- spec/question_paper_spec.rb
+++ spec/question_paper_spec.rb
@@ -2,7 +2,7 @@ require "rspec"
 require "question_paper"
 
 describe QuestionPaper do
-  let(:question) {mock 'Question'}
+  let(:question) { mock 'Question' }
 
   it "should have an empty array of questions when QuestionPaper is created" do
     subject.questions.should be_an_instance_of Array
@@ -14,24 +14,25 @@ describe QuestionPaper do
     subject.questions.should include question
   end
 
-  it "should compute the total available marks" do
-    first_question = mock 'Question'
-    first_question.stub!(:mark).and_return 20
-    subject.add first_question
+  context 'when computing total available marks' do
 
-    second_question = mock 'Question'
-    second_question.stub!(:mark).and_return 10
-    subject.add second_question
+    before do
+      question.stub!(:mark).and_return 10
+      subject.add question
+    end
 
-    subject.total_marks.should == 30
+    it "should compute the total available marks" do
+      second_question = mock 'Question'
+      second_question.stub!(:mark).and_return 20
+      subject.add second_question
+
+      subject.total_marks.should == 30
+    end
+
+    it "should call total_marks on the question in the paper" do
+      question.should_receive(:mark)
+      subject.total_marks
+    end
+
   end
-
-  it "should call total_marks on the question in the paper" do
-    question.stub!(:mark).and_return 10
-    subject.add question
-
-    question.should_receive(:mark)
-    subject.total_marks
-  end
-
 end
